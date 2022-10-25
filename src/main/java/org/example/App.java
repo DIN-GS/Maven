5fdf3e7dc95d36c6e20ea3edd12c473690369ca1
DIN-GS <nikita.ivakin@nure.ua>
2022-10-25 23:10:11 +0300

    maven2

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Car car1 = new Car(100,10,"11.12.2021");
+        System.out.println(car1.isDrivable());
+        System.out.println(car1.distanceCanBeDriven());
+        System.out.println(car1.canBeDriven());
+    }
+}
---
+++ src/test/java/org/example/CarTest.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import lombok.var;
+import org.junit.jupiter.api.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+    @Test
+    void isDrivable() {
+
+        Car car1 = new Car(2,3,"11.12.2021");
+
+        boolean isDriven = car1.getTankFullness() > 0;
+
+        assertEquals(isDriven, car1.isDrivable());
+
+    }
+
+    @Test
+    void canBeDriven() {
+        Car car1 = new Car(2,3,"11.12.2021");
+
+        boolean canBeDriven = car1.canBeDriven();
+
+        assertEquals(true, true);
+    }
+}
